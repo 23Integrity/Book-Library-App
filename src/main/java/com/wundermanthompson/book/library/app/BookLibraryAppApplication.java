1535c5ad030a1c727de743ceb64996f6dfe45ce8
Michał Wesoły <mkwesolt@gmail.com>
2021-04-03 00:24:52 +0200

    Project boilerplate + HTML and styiling
    basic Spring setup with added basic HTML content, styled with some CSS.
    - created index.html site - which is obviously a starting site. Added a navbar, some article-content and a footer
    - created categories.html site - currently containing nothing
    - created topauthors.html site - currently containing nothing

--- src/main/java/com/wundermanthompson/book/library/app/BookLibraryAppApplication.java
+++ src/main/java/com/wundermanthompson/book/library/app/BookLibraryAppApplication.java
@@ -2,12 +2,15 @@ package com.wundermanthompson.book.library.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BookLibraryAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookLibraryAppApplication.class, args);
 	}
-
 }
